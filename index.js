0d7308ff2b5a463a56047877502779a8fc5620f2
taco.chen <taco.chen@tacochende-MacBook-Pro.local>
2017-10-06 14:51:37 +0800

    add-mongodb-connection

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require('express');
-// var https = require("https");
+var https = require("https");
 var bodyParser = require("body-parser");
 var request = require("request");
 var fs = require("fs");
+var mongo = require("mongodb");
+// var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+// var passport = require("passport")
+
+var privateKey = fs.readFileSync('./private.pem', 'utf8');
+var certificate = fs.readFileSync('./file.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var httpsServer = https.createServer(credentials,app);
 var pos = require("./geolocation/far_pos.json");
-var all_pos = require("./data.json")
+var all_pos = require("./data.json");
+var url = "mongodb://localhost:27017/position";
 
 app.set("view engine","ejs");
 app.use(express.static('public'));
@@ -23,7 +33,7 @@ app.get("/geolocation",function(req,res,next){
   };
   request(options,function(err,response,body){
     tmp = body.location;
-    // res.send(body.location);
+    console.log(body);
     res.render("cellid",tmp)
   })
 })
@@ -34,9 +44,35 @@ app.get("/form",function(req,res,next){
 
 app.post("/send",function(req,res,next){
   var tmp = req.body;
-  var temp = fs.readFileSync("./data.json");
-  var json = JSON.parse(temp);
-  json[tmp.ID] = {lat: tmp.lat, lng: tmp.lng};
+  var myobj = {"name":tmp.ID,"position":{"lat":tmp.lat,"lng":tmp.lng}};
+  var myquery = {"name":tmp.ID};
+  var newvalues = {$set:{"position":{"lat":tmp.lat,"lng":tmp.lng}}};
+
+  // var temp = fs.readFileSync("./data.json");
+  // var json = JSON.parse(temp);
+  // json[tmp.ID] = {lat: tmp.lat, lng: tmp.lng};
+  // console.log(json);
+  
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    db.collection("users").findOne({"name": tmp.ID}, function(err, result) {
+      if (err) throw err;
+      if(!result){
+        db.collection("users").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+        });
+      }
+      else{
+        db.collection("users").updateOne(myquery, newvalues, function(err, res) {
+          if (err) throw err;
+          console.log("1 document updated");
+        });
+      }
+      db.close();
+    });
+  });
+
   // if(json.hasOwnProperty(tmp.ID)){
   //   console.log(json);
   //   json[ID].lat = tmp.lat;
@@ -50,17 +86,28 @@ app.post("/send",function(req,res,next){
   //   lat: tmp.lat,
   //   lng: tmp.lng
   // });
-  var obj = JSON.stringify(json);
-  fs.writeFileSync("./data.json",obj);
-  console.log(json);
-  res.end();
+  
+  // var obj = JSON.stringify(json);
+  // fs.writeFileSync("./data.json",obj);
+  // console.log(json);
+  res.end('123');
 })
 
 app.get("/getdata",function(req,res,next){
-  var temp = fs.readFileSync("./data.json");
-  var json = JSON.parse(temp);
-  res.send(json);
-  // res.render("all",json)
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    db.collection("users").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+      res.send(result);
+    });
+  });
+
+  // var temp = fs.readFileSync("./data.json");
+  // var json = JSON.parse(temp);
+  // res.send(json);
+
   // res.send("<div> lat: " + json.position[0].lat +"</div>" + "<div> lng: " + json.position[0].lng + "</div>");
 })
 
@@ -71,6 +118,10 @@ app.get("/marker",function(req,res,next){
 app.get("/current",function(req,res,next){
   res.render("current_pos");
 })
+
+// httpsServer.listen(8081,function(){
+//   console.log("https server on port 8081");
+// })
 
 app.listen(8080,function(){
   console.log("server on port 8080");
