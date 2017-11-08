cba95876555c8aef6e78c04a7f6180291d03c7db
tacochen <kevin09662016@gmail.com>
2017-11-08 16:06:28 +0800

    change mongo url

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const request = require("request");
 const MongoClient = require("mongodb").MongoClient;
 
-const url = "mongodb://localhost:27017/users";
+const url = "mongodb://10.128.21.160:27017/users";
 
 exports.getHomepage = function(req,res,next){
   res.render("index");
